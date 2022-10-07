3569832ad13b9b1a7527aa407fa5250b93e73eca
mohammad <mohammad.goroohi0582@gmail.com>
2022-10-07 20:17:00 +0330

    View pdflinks in browser

---
+++ ArticleManager/wwwroot/MyScripts/OpenArticle.js
@@ -0,0 +1,7 @@
+﻿
+function openArticle(fileName) {
+    let newWindow = window.open(document.location.href);
+    newWindow.addEventListener('DOMContentLoaded', () => {
+        newWindow.document.body.innerHTML = '<embed src="/Articles/' + fileName + '" style="width:100%;height:100%;position: fixed;" type="application/pdf"/>'
+    })
+}
